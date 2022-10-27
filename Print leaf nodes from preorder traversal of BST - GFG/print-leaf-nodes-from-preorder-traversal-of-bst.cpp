81189f3b9e2fa63ffac651a7197523b6d8e8d2ce
Shilesh Kumar <55770859+ShileshKumar@users.noreply.github.com>
2022-10-27 18:14:25 +0530

    Added solution - LeetHub

---
+++ Print leaf nodes from preorder traversal of BST - GFG/print-leaf-nodes-from-preorder-traversal-of-bst.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    vector<int> leafNodes(int arr[],int N) {
+        // code here
+        vector<int> ans;
+        stack<int> st;
+        for (int i = 0; i < N; i++) {
+            int cnt = 0, last = -1;
+            while (!st.empty() and st.top() < arr[i]) {
+                if (last == -1) 
+                    last = st.top(); 
+                st.pop();
+                cnt++;
+            }
+            if (cnt >= 2) 
+                ans.push_back(last);
+            st.push(arr[i]);
+        }
+        ans.push_back(st.top());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        int arr[N];
+        for(int i=0;i<N;i++)cin>>arr[i];
+        Solution ob;
+        vector<int>ans=ob.leafNodes(arr,N);
+        for(int i=0;i<ans.size();i++)cout<<ans[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
