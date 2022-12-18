20581e2773d54a11aefb6c2dc4ac9016f403a01a
Shilesh Kumar <55770859+ShileshKumar@users.noreply.github.com>
2022-12-18 10:32:50 +0530

    Time: 2140 ms (65.85%), Space: 28.4 MB (72.43%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -10,9 +10,9 @@ class Solution(object):
         stack=[n-1]
         ans=[0]
         for pos in range(n-2,-1,-1):
-            while len(stack) > 0 and temperatures[stack[-1]] <= temperatures[pos]:
+            while len(stack)>0 and temperatures[stack[-1]]<=temperatures[pos]:
                 stack.pop()
-            if len(stack) == 0:
+            if len(stack)==0:
                 ans.append(0)
             else:
                 ans.append(stack[-1]-pos)
