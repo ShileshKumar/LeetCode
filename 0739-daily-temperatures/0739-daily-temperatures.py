a1cfbe75ee9c09be4db8150b63c66ae20aaae072
Shilesh Kumar <55770859+ShileshKumar@users.noreply.github.com>
2022-12-18 09:34:48 +0530

    Time: 4507 ms (5.86%), Space: 27.4 MB (98.21%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        n=len(temperatures)
+        if n<=1:
+            return [0]
+        stack=[n-1]
+        ans=[0]
+        for pos in range(n-2,-1,-1):
+            while len(stack) > 0 and temperatures[stack[-1]] <= temperatures[pos]:
+                stack.pop()
+            if len(stack) == 0:
+                ans.append(0)
+            else:
+                ans.append(stack[-1]-pos)
+            stack.append(pos)
+        ans.reverse()
+        return ans
