2ef1fec6166c19743443b149fdfb7b8f5bc870b4
Shilesh Kumar <55770859+ShileshKumar@users.noreply.github.com>
2022-12-22 08:14:45 +0530

    Time: 39 ms (95.94%), Space: 14 MB (32.75%) - LeetHub

---
+++ 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
+        ## dictionary
+        # key: head number of piece
+        # value: all number of single piece
+        mapping = { piece[0]: piece for piece in pieces }
+        
+        result = []
+        
+        # try to make array from pieces
+        for number in arr:
+            
+            result += mapping.get( number, [] )
+        
+        # check they are the same or not
+        return result == arr
