1c384e9a7daafa6074c2029bd2ca605165ff17b9
Shilesh Kumar <55770859+ShileshKumar@users.noreply.github.com>
2023-03-01 08:33:00 +0530

    Time: 1983 ms (43.68%), Space: 21.7 MB (75.96%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,5 +1,6 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
+        #n=len(nums)//2
         if len(nums)>1:
             p=len(nums)//2
             left=nums[:p]
@@ -8,25 +9,28 @@ class Solution:
             self.sortArray(left)
             self.sortArray(right)
             
-            i=j=k=0
+            i=0
+            j=0
+            k=0
             
             while i<len(left) and j<len(right):
                 if left[i]<right[j]:
                     nums[k]=left[i]
                     i+=1
+                    k+=1
                 else:
                     nums[k]=right[j]
                     j+=1
-                k+=1
-                
+                    k+=1
+            
             while i<len(left):
                 nums[k]=left[i]
                 i+=1
                 k+=1
-                
+            
             while j<len(right):
                 nums[k]=right[j]
                 j+=1
                 k+=1
-                
+            
         return nums
